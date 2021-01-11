08cf641b9484e3fb5aa88f3849cdffb85063ba2a
gaoliang.z <gaoliang.z@bytedance.com>
2021-01-11 18:29:08 +0800

    choer: 添加注释

--- src/features/global.js
+++ src/features/global.js
@@ -1,5 +1,6 @@
 import { systemJSPrototype } from '../system-core.js';
 
+// exec 事件一定在 prepareImport 之后发生，所以即便 prepareImport 是异步的，subscribe 也不会错过自己 package 的 exec 事件
 systemJSPrototype.registerGlobal = function (importKey, globalName) {
   if (typeof System.subscribe === 'undefined') {
     console.error('System.subscribe is not available, failed to expose ${globalName} to global');
